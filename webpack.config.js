e7b84fac43b71dd7b09e4ef62d6ef2e16b741c79
Ali <best_web@ymail.com>
2018-12-07 20:58:23 +0330

    config babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "fonts/[hash].[ext]",
+            limit: 5000,
+            mimetype: "application/font-woff"
+          }
+        }
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[hash].[ext]"
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./dist/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+    port: 3000,
+    historyApiFallback: true
+  }
+};
