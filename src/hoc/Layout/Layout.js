d511f109883db854eb6f7546230604190070d025
Ali <best_web@ymail.com>
2018-12-08 17:30:14 +0330

    Change Layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Tag } from "@blueprintjs/core";
 import "./Layout.scss";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 
@@ -12,7 +12,8 @@ class Layout extends Component {
         <div className="page">
         <div className="pageHeader">
           <h1>HTML to PDF</h1>
-          <h2>Convert web pages or HTML files to PDF documents</h2>
+          <h2>Convert HTML codes or HTML files to PDF documents</h2>
+          <Tag>Version 0.1.0</Tag>
         </div>
         {this.props.children}</div></div>
       </main>
